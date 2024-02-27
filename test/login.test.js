c42a4d14ef34f2c8a6d9563e98f9a58b65b35430
roLLlyTHuK <ircvipua@gmail.com>
2024-02-27 10:42:17 +0200

    add login test

---
+++ test/login.test.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import dotenv from "dotenv";
+dotenv.config();
+import app from "../app.js";
+import { userControllers } from "../controllers/auth.js";
+
+const { DB_URI } = process.env;
+
+const ENDPOINT = "/users/login";
+const TEST_USER = {
+  email: "test@test.ua",
+  password: "test@test.ua",
+};
+
+describe("test login controller", () => {
+  beforeAll(async () => {
+    await mongoose.connect(DB_URI);
+  });
+  afterAll(async () => {
+    await mongoose.disconnect(DB_URI);
+  });
+
+  test("login controller returns status code 200", async () => {
+    const response = await request(app).post(ENDPOINT).send(TEST_USER);
+    expect(response.status).toBe(200);
+  });
+  test("login controller returns token", async () => {
+    const response = await request(app).post(ENDPOINT).send(TEST_USER);
+    expect(response.body.token).toBeDefined();
+  });
+  test("login controller returns object user with 2 fields: email and subscription with typeof string", async () => {
+    const response = await request(app).post(ENDPOINT).send(TEST_USER);
+
+    expect(response).toBeDefined();
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body.user).toMatchObject({
+      email: expect.any(String),
+      subscription: expect.any(String),
+    });
+  });
+});
